158034ff596b05ea733dc433f92f6c268a349331
Deybson Ferreira <deybson.ferreira@iatec.com>
2022-12-01 14:55:53 -0300

    comit

--- src/Biblioteca/Operacoes/Salvar.java
+++ src/Biblioteca/Operacoes/Salvar.java
@@ -11,15 +11,21 @@ public class Salvar {
     public static void SalvarNovoDado(String novaLinha, String url, String cabecalho) {
 
         List<String> listaExcel = new ArrayList<String>();
-
+        String primeiraLinha = "";
         try {
             // buscar linhas no csv
             listaExcel = ManipulaArquivo.leitor(url);
-        } catch (IOException e) {
+            primeiraLinha = listaExcel.get(0);
+        } catch (Exception e) {
 
         }
+
+        // quando não contém cabeçalho no arquivo > adiciona cabecalho
+        if (!primeiraLinha.contains(cabecalho.split(";")[0])) {
+            listaExcel.add(cabecalho);
+        }
+
         // adicionar nova linha dentro das lista existente
-        listaExcel.add(cabecalho);
         listaExcel.add(novaLinha);
 
         // salvar toda lista no csv ( com o dado novo)
